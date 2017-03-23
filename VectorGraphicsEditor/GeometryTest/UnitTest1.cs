5f5d057e4d7cb53ec3aa35d1675b136dc727cad1
Semargl23 <mamykin.2013@yandex.com>
2017-03-23 11:15:11 +0700

    Add files via upload

--- VectorGraphicsEditor/GeometryTest/UnitTest1.cs
+++ VectorGraphicsEditor/GeometryTest/UnitTest1.cs
@@ -12,28 +12,9 @@ namespace GeometryTest
         [TestMethod]
         public void TestMethod1()
         {
-            /*
-            Rectangle TestFigure = new Rectangle(
-                                                new Point(1,1),
-                                                new Point(10,10),
-                                                new Color(0, 0, 0, 1),
-                                                new Color(255, 255, 255, 1));
-             Конструктор работает
-           */
-
-            
-            FigureFactory Factory = new FigureFactory();
-
-            var TestRect = Factory.Create("Rectangle", new Dictionary<string, object>()
-            {
-                ["DownLeft"] = new Point(1, 1),
-                ["UpRight"] = new Point(10, 10),
-                ["BorderColor"] = new Color(0, 0, 0, 1),
-                ["FillColor"] = new Color(255, 255, 255, 1)
-            });
-
-            FigureFactory B = new FigureFactory();
+            var TestTriangulation = new Test();
 
         }
+       
     }
 }
